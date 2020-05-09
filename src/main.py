848d90b878d8e193d719ebf8ae14ef44ad6ff807
Hal Anil <haldun.anil@braze.com>
2020-05-09 16:52:01 -0400

    Turn scripts into modules

---
+++ src/main.py
@@ -0,0 +1,6 @@
+from get_testing_data import get_testing_data
+from parse_hospitalizations_and_deaths import parse_hospitalizations_and_deaths
+
+if __name__ == "__main__":
+    get_testing_data()
+    parse_hospitalizations_and_deaths()
